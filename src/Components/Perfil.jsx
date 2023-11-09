ddfcc40c5da6fe66e2b1c1a252551739578a6bfd
Nicolrayen <nicolaros95@gmail.com>
2023-11-09 19:14:48 -0300

    first commit

---
+++ src/Components/Perfil.jsx
@@ -0,0 +1,13 @@
+// src/components/Perfil.js
+
+
+function Perfil() {
+    return (
+        <div id="perfil">
+            <h2>Perfil de Usuario</h2>
+            {/* Mostrar información del perfil y actividades del usuario */}
+        </div>
+    );
+}
+
+export default Perfil;
